4df19fa7f56d403cc45e1143ff5567d85b25d7c8
Voroz <jonny_fherm@hotmail.com>
2016-11-07 21:16:11 +0100

    Commit

--- GameServer/SyncManager.h
+++ GameServer/SyncManager.h
@@ -14,7 +14,8 @@ public:
 	int addObject(SyncObject<void*>& obj);
 	int removeObject(SyncObject<void*>& obj);
 	SyncObject<void*>& inControlObjects(Client* client); // TODO: Send these objects to their clients if values were changed
-	SyncObject<void*>& toBeSyncedObjects(Client* client); // TODO: in client.receive(), sync the objects where client ids and obj ids match
+	SyncObject<void*>& toBeSyncedObjects(); // TODO: in client.receive(), sync the objects where client ids and obj ids match
+	void syncObjects(); // Use this function in server.receive()
 
 private:
 	std::vector<SyncObject<void*>*> _syncObjects;
